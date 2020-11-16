bb6d485f68574a285d70ce55cdbac6c8ca617010
Lance.Lan <lance.lan@bulletin.net>
2020-11-16 21:24:18 +1300

    add command description

--- src/main/java/com/lance/motiondesigninterview/MotionDesignInterviewApplication.java
+++ src/main/java/com/lance/motiondesigninterview/MotionDesignInterviewApplication.java
@@ -24,6 +24,13 @@ public class MotionDesignInterviewApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.println("This tool provides to packing algorithm. Please type in packing criteria and items in the following format.\n" +
+				"#Input format:\n" +
+				"[Sort order],[max pieces per pack],[max weight per pack]\n" +
+				"[item id],[item length],[item quantity],[piece weight]\n" +
+				"[item id],[item length],[item quantity],[piece weight]\n" +
+				"[item id],[item length],[item quantity],[piece weight]\n" +
+				"Please past your input below and type Enter key:\n");
 		PackingCommand packingCommand = this.packingCommandParser.parseCommand(System.in);
 		List<Pack> packs = packer.packItems(packingCommand);
 		packs.forEach(Pack::print);
